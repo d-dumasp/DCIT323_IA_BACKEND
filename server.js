53cb3144329f60b83a7579bfd04160ebff153a4d
dpabby-kwasi <pabbykwasi@gmail.com>
2026-05-05 16:48:13 +0000

    updated server

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173', // Vite default port
+    origin: [ 'http://localhost:5173', process.env.FRONTEND_URL ],
     credentials: true
 }));
 
